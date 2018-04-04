ada711bb1bd15727c8536f8dce41949a986b7bc1
Robyn Dessoy <focks@live.co.uk>
2018-04-04 16:18:57 +0100

    added hello world command and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # time-tracking-bot
 Bot to track time
+
+Heavily influenced and helped by James Seden Smith, creator of Phial (https://github.com/sedders123/phial) which the Time Tracking Bot also uses.
--- trackingbot/commands.py
+++ trackingbot/commands.py
@@ -4,5 +4,10 @@ from threading import Thread, Lock
 from time import sleep
 from fractions import Fraction
 from trackingbot import bot
+from config import TEST_TRACKING_CHANNEL_ID
 
 #making changes to add develop branch
+
+@bot.command("echo")
+def echo():
+	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text="Hello World."))
