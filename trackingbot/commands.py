551cfd034665795eae01220e4b15f22e2ccacc31
Robyn Dessoy <focks@live.co.uk>
2018-04-05 09:03:36 +0100

    added datetime commands and started csv helpers

--- trackingbot/commands.py
+++ trackingbot/commands.py
@@ -5,9 +5,29 @@ from time import sleep
 from fractions import Fraction
 from trackingbot import bot
 from config import TEST_TRACKING_CHANNEL_ID
+from trackingbot.csvs import get_date, get_date_time
 
 #making changes to add develop branch
 
-@bot.command("echo")
-def echo():
-	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text="Hello World."))
+@bot.command("echo <text>")
+def echo(text):
+	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text=text))
+	
+
+@bot.command("start <task_name>")
+def start(task_name):
+	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text=task_name + " started"))
+	
+@bot.command("finish")
+def finish():
+	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text="Task finished"))
+	
+@bot.command("date")
+def date():
+	date = get_date()
+	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text=date))
+	
+@bot.command("datetime")
+def datetime():
+	datetime = get_date_time()
+	bot.send_message(Response(TEST_TRACKING_CHANNEL_ID, text=datetime))
