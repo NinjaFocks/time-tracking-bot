96d3cd8d141f40569df91902949183012f2e0dcf
Robyn Dessoy <focks@live.co.uk>
2018-04-13 15:17:24 +0100

    working on csv methods - have create file working, now working on start task method

--- trackingbot/csvs.py
+++ trackingbot/csvs.py
@@ -1,22 +1,29 @@
+import csv
 from trackingbot.dates import get_date, get_time
 	
 def create_file():
 	date = get_date()
-	with open(date + '.csv', 'wb') as csvFile:
-		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	with open(date + '.csv', 'w') as csvFile:		
+		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
 		filewriter.writerow(['TaskName', 'StartedTime', 'FinishedTime','Progress'])
+		csvFile.close()
+	
 		
 def start_task(task_name):
 	date = get_date()
 	time = get_time()
-	with open(date + '.csv') as csvFile:
-		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		filewriter.writerow([task_name, time, '', 'InProgress'])
+	create_file()
+	csvFile = open(date + '.csv', 'a')
+	print('opened file')
+	filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
+	filewriter.writerow([task_name, time, '', 'InProgress'])
+	csvFile.close()
+	print('closed file')
 		
-def finish_task(task_name)
-	date = get_date()
-	time = get_time()
-	file = csv.reader(open(date + '.csv', 'rb'), delimiter-',')
-	for row in file:
-		if 
+# def finish_task(task_name):
+	# date = get_date()
+	# time = get_time()
+	# file = csv.reader(open(date + '.csv', 'rb'), delimiter-',')
+	# for row in file:
+		# if 
 		
