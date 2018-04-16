15c92589d87437d763a6ecf5300f8f6441bd48cd
Robyn Dessoy <focks@live.co.uk>
2018-04-16 09:04:07 +0100

    finished working on start command

--- trackingbot/csvs.py
+++ trackingbot/csvs.py
@@ -1,9 +1,12 @@
 import csv
 from trackingbot.dates import get_date, get_time
+from pathlib import Path
+import sys
 	
 def create_file():
 	date = get_date()
-	with open(date + '.csv', 'w') as csvFile:		
+	print('creating file')
+	with open('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv', 'w', newline='') as csvFile:		
 		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
 		filewriter.writerow(['TaskName', 'StartedTime', 'FinishedTime','Progress'])
 		csvFile.close()
@@ -11,19 +14,23 @@ def create_file():
 		
 def start_task(task_name):
 	date = get_date()
-	time = get_time()
-	create_file()
-	csvFile = open(date + '.csv', 'a')
+	time = get_time()	
+	
+	today_file = Path('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv')
+	if (not today_file.is_file()):
+		create_file()	
+	print('opening file')
+	csvFile = open('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv', 'a', newline='')	
 	print('opened file')
 	filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
 	filewriter.writerow([task_name, time, '', 'InProgress'])
 	csvFile.close()
 	print('closed file')
 		
-# def finish_task(task_name):
-	# date = get_date()
-	# time = get_time()
-	# file = csv.reader(open(date + '.csv', 'rb'), delimiter-',')
-	# for row in file:
-		# if 
-		
+ #def finish_task(task_name):
+	#date = get_date()
+	#time = get_time()
+	#file = csv.reader(open('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv', 'r'), delimiter=',')
+	#for row in file:
+		#if (task_name == row[0]):
+			
