d899c4464323c8fe8ada8410108d17bf1996a32a
NinjaFocks <NinjaFocks@users.noreply.github.com>
2018-04-24 10:25:02 +0100

    id:10 finished finish command (#5)

--- trackingbot/csvs.py
+++ trackingbot/csvs.py
@@ -1,12 +1,13 @@
 import csv
 from trackingbot.dates import get_date, get_time
 from pathlib import Path
-import sys
+import shutil
+from config import CSV_FILE_LOCATION
 	
 def create_file():
 	date = get_date()
 	print('creating file')
-	with open('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv', 'w', newline='') as csvFile:		
+	with open(CSV_FILE_LOCATION + date + '.csv', 'w', newline='') as csvFile:		
 		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
 		filewriter.writerow(['TaskName', 'StartedTime', 'FinishedTime','Progress'])
 		csvFile.close()
@@ -16,21 +17,37 @@ def start_task(task_name):
 	date = get_date()
 	time = get_time()	
 	
-	today_file = Path('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv')
+	today_file = Path(CSV_FILE_LOCATION + date + '.csv')
 	if (not today_file.is_file()):
 		create_file()	
-	print('opening file')
-	csvFile = open('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv', 'a', newline='')	
-	print('opened file')
-	filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
-	filewriter.writerow([task_name, time, '', 'InProgress'])
-	csvFile.close()
-	print('closed file')
+	else:
+		finish_task()
 		
- #def finish_task(task_name):
-	#date = get_date()
-	#time = get_time()
-	#file = csv.reader(open('C:/Git/time-tracking-bot/trackingbot/csvFiles/' + date + '.csv', 'r'), delimiter=',')
-	#for row in file:
-		#if (task_name == row[0]):
-			
+	with open(CSV_FILE_LOCATION + date + '.csv', 'a', newline='') as csvFile:
+		print('start_task opened file')
+		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
+		filewriter.writerow([task_name, time, '', 'InProgress'])		
+	print('start_task closed file')
+		
+def finish_task():
+	date = get_date()
+	time = get_time()
+	
+	csvLocation = CSV_FILE_LOCATION + date + '.csv'
+	tempLocation = CSV_FILE_LOCATION + date + '_temp.csv'
+	
+	with open(csvLocation, 'r', newline='') as csvFile, open(tempLocation, 'w', newline='') as tempfile:	
+		filereader = csv.reader(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		filewriter = csv.writer(tempfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		print('finish_task opened file')
+	
+		for row in filereader:
+			if (row[3] == 'InProgress'):
+				row[2] = time
+				row[3] = 'Finished'
+				filewriter.writerow(row)			
+			else: 
+				filewriter.writerow(row)
+	
+	print('finish_task closed file')
+	shutil.move(tempfile.name, csvFile.name)
