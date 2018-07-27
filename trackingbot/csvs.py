885d2f18666f31d022ae9fc1825a765213cf5475
NinjaFocks <NinjaFocks@users.noreply.github.com>
2018-07-27 09:20:27 +0100

    id:15 add time to row (#8)
    * added time calculation
    
    * updated summary command

--- trackingbot/csvs.py
+++ trackingbot/csvs.py
@@ -10,7 +10,7 @@ def create_file():
 	print('creating file')
 	with open(CSV_FILE_LOCATION + date + '.csv', 'w', newline='') as csvFile:		
 		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
-		filewriter.writerow(['TaskName', 'StartedTime', 'FinishedTime','Progress'])
+		filewriter.writerow(['TaskName', 'StartedTime', 'FinishedTime','Progress', 'TimeSpent'])
 		csvFile.close()
 	
 		
@@ -27,7 +27,7 @@ def start_task(task_name):
 	with open(CSV_FILE_LOCATION + date + '.csv', 'a', newline='') as csvFile:
 		print('start_task opened file')
 		filewriter = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)		
-		filewriter.writerow([task_name, time, '', 'InProgress'])		
+		filewriter.writerow([task_name, time, '', 'InProgress', 0])		
 	print('start_task closed file')
 		
 def finish_task():
@@ -46,6 +46,7 @@ def finish_task():
 			if (row[3] == 'InProgress'):
 				row[2] = time
 				row[3] = 'Finished'
+				row[4] = datetime.strptime(time, '%H:%M') - datetime.strptime(row[1], '%H:%M')
 				filewriter.writerow(row)			
 			else: 
 				filewriter.writerow(row)
@@ -63,13 +64,10 @@ def get_summary():
 		print('summary opened file')
 		filereader = csv.reader(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)	
 		for row in filereader:
-			if (row[0] == 'TaskName'):
-				summary = str(row[0]) + ' - ' + 'Time Taken\n'				
-			elif (row[3] == 'InProgress'):
+			if (row[3] == 'InProgress'):
 				summary = summary + row[0] + ' - ' + 'In Progress' + '\n'			 
 			else:
-				time = datetime.strptime(row[2], '%H:%M') - datetime.strptime(row[1], '%H:%M')
-				summary = summary + row[0] + ' - ' + str(time) + '\n'
+				summary = summary + row[0] + ' - ' + row[4] + '\n'
 	print('summary closed file')	
 	return summary
 			
